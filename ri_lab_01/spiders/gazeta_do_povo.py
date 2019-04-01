b38ec9d9fe73394fe4a788f88fb191a6c45d6de6
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2019-04-01 18:24:23 -0300

    Set CSS selectors

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -13,19 +13,21 @@ class GazetaDoPovoSpider(scrapy.Spider):
 
     def __init__(self, *a, **kw):
         super(GazetaDoPovoSpider, self).__init__(*a, **kw)
-        with open('frontier/gazetadopovo.json') as json_file:
-                data = json.load(json_file)
+        with open('seeds/gazetadopovo.json') as json_file:
+            data = json.load(json_file)
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        item = RiLab01Item()
+        item['_id'] = '1'
+        item['title'] = response.css('h1.c-titulo::text').get()
+        item['sub_title'] = response.css('h3.c-sobretitulo span::text').get()
+        item['author'] = response.css('li.c-autor span::text').get()
+        item['date'] = response.css('li.data-publicacao time::text').get()
+        item['section'] = response.xpath('/html/body/div[2]/section[2]/section[1]/ol/li[2]/a/span/text()').extract_first()
+        item['text'] = response.css('h2.c-sumario::text').get()
+        item['url'] = response.url
+
+        yield item
+
+
