d0ed720c443c650ee22c83edf525d62c19d5a63f
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2019-04-01 18:28:42 -0300

    Improve CSS selectors

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -9,23 +9,23 @@ from ri_lab_01.items import RiLab01CommentItem
 class GazetaDoPovoSpider(scrapy.Spider):
     name = 'gazeta_do_povo'
     allowed_domains = ['gazetadopovo.com.br']
-    start_urls = []
+    start_urls = ['https://www.gazetadopovo.com.br/economia/como-esse-americano-conseguiu-desaparecer-da-web-em-15-passos-1xl7117h4actrfg3zbtqjittq/']
 
-    def __init__(self, *a, **kw):
-        super(GazetaDoPovoSpider, self).__init__(*a, **kw)
-        with open('seeds/gazetadopovo.json') as json_file:
-            data = json.load(json_file)
-        self.start_urls = list(data.values())
+ #   def __init__(self, *a, **kw):
+    #    super(GazetaDoPovoSpider, self).__init__(*a, **kw)
+    #    with open('seeds/gazetadopovo.json') as json_file:
+    #            data = json.load(json_file)
+    #    self.start_urls = list(data.values())
 
     def parse(self, response):
         item = RiLab01Item()
         item['_id'] = '1'
-        item['title'] = response.css('h1.c-titulo::text').get()
-        item['sub_title'] = response.css('h3.c-sobretitulo span::text').get()
-        item['author'] = response.css('li.c-autor span::text').get()
-        item['date'] = response.css('li.data-publicacao time::text').get()
+        item['title'] = response.css('.c-titulo::text').get()
+        item['sub_title'] = response.css('.c-sobretitulo span::text').get()
+        item['author'] = response.css('.c-autor span::text').get()
+        item['date'] = response.css('.data-publicacao time::text').get()
         item['section'] = response.xpath('/html/body/div[2]/section[2]/section[1]/ol/li[2]/a/span/text()').extract_first()
-        item['text'] = response.css('h2.c-sumario::text').get()
+        item['text'] = response.css('.c-sumario::text').get()
         item['url'] = response.url
 
         yield item
