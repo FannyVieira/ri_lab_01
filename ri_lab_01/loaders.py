b19fead918036a85f46961cdcd419d66f89cdd6f
FannyVieira <fanny.vieira@ccc.ufcg.edu.br>
2019-04-03 15:14:31 -0300

    Add join for text

--- ri_lab_01/loaders.py
+++ ri_lab_01/loaders.py
@@ -1,6 +1,8 @@
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst
+from scrapy.loader.processors import TakeFirst, Join
+
 
 class RiLab01Loader(ItemLoader):
 
     default_output_processor = TakeFirst()
+    text_out = Join()
