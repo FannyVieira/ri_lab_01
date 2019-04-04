aae1ca4cbdaf8b7ba628bd53116e2485abe8bbd1
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2019-04-03 21:05:56 -0300

    Refactor spider

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -6,8 +6,9 @@ from urllib.parse import urlsplit, urljoin
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
 from ri_lab_01.loaders import RiLab01Loader
-from ri_lab_01.settings import DEADLINE
-from datetime import date
+from ri_lab_01.util.date import is_valid_date
+from ri_lab_01.util.file import write_in_frontier
+from ri_lab_01.util.seletor import get_value_by_selector
 
 
 class GazetaDoPovoSpider(scrapy.Spider):
@@ -22,9 +23,13 @@ class GazetaDoPovoSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        active_page = int(response.css('.pg-ativa::text').get())
+        urls_sections = [a.attrib['href'] for a in get_value_by_selector(response,'.c-noticias-rapidas a')]
+        for url in urls_sections:
+            yield scrapy.Request(url, callback=self.parse_pagination_page)
+
+    def parse_pagination_page(self, response):
         page_request_count = 0
-        last_news_links = [a.attrib['href'] for a in response.css('.ultimas-chamadas a') if self.is_valid_date(a.attrib['data-publication'])]
+        last_news_links = [a.attrib['href'] for a in get_value_by_selector(response,'.c-chamada a')]
 
         for url in last_news_links:
             page_request_count += 1
@@ -32,9 +37,8 @@ class GazetaDoPovoSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_news_page, meta={'page_count': page_request_count})
 
         if last_news_links:
-            new_page = active_page + 1
-            updated_url = urlsplit(response.url)._replace(query='offset={new_page}'.format(new_page=new_page)).geturl()
-            yield scrapy.Request(updated_url, self.parse)
+            updated_url = self.get_new_url_by_pagination(response)
+            yield scrapy.Request(updated_url, self.parse_pagination_page)
 
     def parse_news_page(self, response):
         loader = RiLab01Loader(item=RiLab01Item(), response=response)
@@ -43,7 +47,6 @@ class GazetaDoPovoSpider(scrapy.Spider):
         loader.add_value('_id', response.meta.get('page_count'))
         loader.add_css('title', '.c-titulo::text')
         loader.add_css('title', '.c-title::text')
-        loader.add_css('title', '[class*="title"]')
         loader.add_css('sub_title', '.c-sobretitulo span::text')
         loader.add_css('sub_title', 'c-overhead span::text')
         loader.add_css('author', '[class*="autor"] span::text')
@@ -53,26 +56,15 @@ class GazetaDoPovoSpider(scrapy.Spider):
         loader.add_value('section', url.split('/')[3])
         loader.add_css('text', 'p::text')
         loader.add_value('url', url)
-
-        self.write_in_frontier(loader)
+        write_in_frontier(loader)
 
         return loader.load_item()
 
-    def write_in_frontier(self, loader):
-        with open('frontier/gazeta_do_povo.json', 'a') as json_file:
-            title = loader.get_output_value('_id')
-            url = loader.get_output_value('url')
-            data = {title: url}
-            json.dump(data, json_file)
+    def get_new_url_by_pagination(self, response):
+        active_page = int(get_value_by_selector(response,'.pg-ativa::text').get())
+        new_page = active_page + 1
+        updated_url = urlsplit(response.url)._replace(query='offset={new_page}'.format(new_page=new_page)).geturl()
 
-    def is_valid_date(self, value):
-        DAY_DEADLINE, MONTH_DEADLINE, YEAR_DEADLINE = DEADLINE.split('.')
-        DEADLINE_DATE = date(int(YEAR_DEADLINE), int(MONTH_DEADLINE), int(DAY_DEADLINE))
-
-        # A data extraída está no seguinte formato: YYYY-MM-DD
-        year, month, day = value.split('-')
-        pub_date = date(int(year), int(month), int(day))
-
-        return pub_date > DEADLINE_DATE
+        return updated_url
 
 
