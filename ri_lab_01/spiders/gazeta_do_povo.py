d3289f3c09275ae1b13f60cb3e029a160a17fc7f
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2019-04-05 08:18:58 -0300

    Finish comments

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -23,11 +23,20 @@ class GazetaDoPovoSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
+        """Get the list of urls from sections of the Gazeta's site(e.g 'ultimas-noticias', 'populares', etc)
+        :param response: the html response from scrapy download
+        :return scrapy.Item: a iterable list of items extracted
+        """
         urls_sections = [a.attrib['href'] for a in get_value_by_selector(response,'.c-noticias-rapidas a')]
         for url in urls_sections:
             yield scrapy.Request(url, callback=self.parse_pagination_page)
 
     def parse_pagination_page(self, response):
+        """Get a list of news URL and set pagination logic to get more, see this page
+        to understand better(https://www.gazetadopovo.com.br/ultimas-noticias/)
+        :param response: the html response from scrapy download
+        :return scrapy.Item: a iterable list of items extracted
+        """
         page_request_count = 0
         news_links = [a.attrib['href'] for a in get_value_by_selector(response,'.c-chamada a')]
 
@@ -42,6 +51,12 @@ class GazetaDoPovoSpider(scrapy.Spider):
             yield scrapy.Request(updated_url, self.parse_pagination_page)
 
     def parse_news_page(self, response):
+        """Parse the news page and extract text, title, subtitle, author,
+        section(the section is part of url), date and url data using loader object,
+        check more in documentation(https://docs.scrapy.org/en/latest/topics/loaders.html)
+        :param response: the html response from scrapy download
+        :return scrapy.Item: a iterable list of items extracted
+        """
         loader = RiLab01Loader(item=RiLab01Item(), response=response)
         url = response.url
 
@@ -62,6 +77,12 @@ class GazetaDoPovoSpider(scrapy.Spider):
         return loader.load_item()
 
     def parse_comments_section(self, response):
+        """Parse the news page and extract data about comments as author, date, text,
+        using loader object, check more in documentation
+        (https://docs.scrapy.org/en/latest/topics/loaders.html)
+        :param response: the html response from scrapy download
+        :return scrapy.Item: a iterable list of items extracted
+        """
         loader = RiLab01CommentLoader(item=RiLab01CommentItem(), response=response)
         loader.add_value('id_article', '')
         loader.add_css('author', '.user-name::text')
@@ -72,6 +93,11 @@ class GazetaDoPovoSpider(scrapy.Spider):
 
 
     def get_new_url_by_pagination(self, response):
+        """Get a new url through pagination, the difference necessary is change a
+        'offset' atributte in query URL incrementing the number of current page.
+        :param response: the html response from scrapy download
+        :return str: a new url result of pagination
+        """
         active_page = int(get_value_by_selector(response,'.pg-ativa::text').get())
         new_page = active_page + 1
         updated_url = urlsplit(response.url)._replace(query='offset={new_page}'.format(new_page=new_page)).geturl()
